a2f4fa2690f576b7b52f792366002153e7ea4784
Md Adnan Akhtar <64841826+Adnan7786@users.noreply.github.com>
2020-09-18 12:15:37 +0530

    Add files via upload

---
+++ adscript.js
@@ -0,0 +1,287 @@
+// Language and Genre Button
+function myAccFunc(j) {
+ //Language Button
+ if (j==1) {
+	 
+	 //hide genre dropdown if it's open
+    var x = document.getElementById("allGenre");
+    if (x.className.indexOf("ad-show") != -1) {
+		x.className = x.className.replace(" ad-show", "");
+	}
+	
+    //show or hide language dropdown 	
+   var y = document.getElementById("allLanguage");
+   if (y.className.indexOf("ad-show") == -1) {
+     y.className += " ad-show";
+   } else {
+     y.className = y.className.replace(" ad-show", "");
+   }
+  
+ }
+ //Genre Button
+ if (j==2) {
+	 
+	//hide language dropdown if it's open
+    var x = document.getElementById("allLanguage");
+    if (x.className.indexOf("ad-show") != -1) {
+		x.className = x.className.replace(" ad-show", "");
+	}	
+    //show/hide genre dropdown 	
+   var y = document.getElementById("allGenre");
+   if (y.className.indexOf("ad-show") == -1) {
+     y.className += " ad-show";
+   } else {
+     y.className = y.className.replace(" ad-show", "");
+   }
+  
+ }
+}
+
+// Open and close sidebar
+function ad_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function ad_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+
+
+//Song Class Definition
+class Song{
+	constructor(n,a,l,g){
+		this.name = n;
+		this.artist= a;
+		this.language= l;
+		this.genre= g;
+		this.srce= "allSongs/" + this.name + ".mp3";
+		this.lrc="lyrics/" + this.name + ".txt";
+	}
+}
+
+//array of all songs
+var songs=[];
+function initialiseSongs(){
+	//all Songs object pushed to song list
+     
+	
+	songs.push(new Song("12 Saal",["Bilal Saeed"],"Punjabi",["Pop"]));	
+	//A
+	songs.push(new Song("Allahu Allah",["Sami Yusuf"],"Arabic",["Sufi"]));
+	songs.push(new Song("Amplifier",["Imran Khan"],"Punjabi",["Pop"]));
+	songs.push(new Song("Ashq Na Ho",["Arijit Singh"],"Hindi",["Bollywood"]));
+	songs.push(new Song("Aye Khuda",["Mithoon","Kshitij Tarey","Saim"],"Hindi",["Pop"]));
+	//B
+	songs.push(new Song("Beautiful",["Akon"],"English",["Pop"]));
+	songs.push(new Song("Bella Ciao",["Manu Pilas"],"Italian",["Folk"]));
+	songs.push(new Song("Bewafa",["Imran Khan"],"Punjabi",["Pop"]));
+	songs.push(new Song("Bhar Do Jholi Meri",["Adnan Sami"],"Hindi",["Sufi","Bollywood"]));
+	songs.push(new Song("Birthday Bash",["Yo Yo Honey Singh","Alfaaz"],"Hindi",["Bollywood","Hip Hop"]));
+	songs.push(new Song("Breathless",["Shankar Mahadevan"],"Hindi",["Pop"]));
+	songs.push(new Song("Brown Rang",["Honey Singh"],"Punjabi",["Hip Hop"]));
+	songs.push(new Song("Budapest",["George Ezra"],"English",["Pop"]));
+
+	//C
+	songs.push(new Song("Con Altura",["Rosalia","J Balvin","El Guincho"],"Spanish",["Hip Hop"]));
+
+	//D
+	songs.push(new Song("Dangerous",["Kardinall Offishall", "Akon"],"English",["Hip Hop"]));
+	songs.push(new Song("Das Dae",["Mohit Chauhan","Jaidev Kumar"],"Punjabi",["Pop"]));
+	songs.push(new Song("Despacito",["Luis Fonsi","Daddy Yankee"],"Spanish",["Hip Hop"]));
+	songs.push(new Song("Dope Shope",["Yo Yo Honey Singh"],"Punjabi",["Hip Hop"]));
+
+	//E
+
+	//F
+
+	//G
+	songs.push(new Song("Give Me Some Sunshine",["Suraj Jagan", "Sharman Joshi"],"Hindi",["Bollywood"]));
+
+	//H
+	songs.push(new Song("Ham Madine Mein",["Waheed Zafar Qasmi"],"Hindi",["Sufi"]));
+	songs.push(new Song("House of Gold",["Twenty one Pilots"],"English",["Pop","Folk Rock"]));
+
+	//I
+
+	//J
+	songs.push(new Song("Jee Le Zara",["Vishal Dadlani"],"Hindi",["Pop"]));
+
+	//K
+	songs.push(new Song("Kun Faya Kun",["A.R Rahman", "Javed Ali","Mohit Chauhan"],"Hindi",["Bollywood","Sufi"]));
+
+	//L
+	songs.push(new Song("Lab Pe Aati Hai Dua",["Siza Roy"],"Hindi",["Pop"]));
+	songs.push(new Song("Laden",["Jassi Gill"],"Punjabi",["Pop"]));
+	songs.push(new Song("Let Her Go",["Passenger"],"English",["Folk Rock"]));
+	songs.push(new Song("Lo Maan Liya",["Arijit Singh"],"Hindi",["Bollywood"]));
+	songs.push(new Song("Lose Yourself",["Eminem"],"English",["Hip Hop"]));
+
+	//M
+	songs.push(new Song("Main Dhoondne Ko",["Arijit Singh"],"Hindi",["Bollywood"]));
+	songs.push(new Song("Mia",["Bad Bunny","Drake"],"Spanish",["Hip Hop"]));
+	songs.push(new Song("Migraine",["Twenty One Pilots"],"English",["Pop"]));
+
+	//N
+	songs.push(new Song("Nada",["Chela Rivas","Tony Dark Eyes"],"Spanish",["Pop"]));
+	songs.push(new Song("Nadaan Parindey",["A.R Rahman"],"Hindi",["Bollywood"]));
+
+	//O
+	songs.push(new Song("One Bottle Down",["Yo Yo Honey Singh"],"Hindi",["Bollywood","Hip Hop"]));
+
+	//P
+
+	//Q
+
+	//R
+
+	//S
+	songs.push(new Song("Sigueme Y Te Siego",["Daddy Yankee"],"Spanish",["Hip Hop"]));
+	songs.push(new Song("Someone You Loved",["Lewis Capaldi"],"English",["Pop"]));
+	songs.push(new Song("Stressed Out",["Twenty One Pilots"],"English",["Rock","Hip Hop"]));
+	songs.push(new Song("Sunflower",["Post Malone", "Swae Lee"],"English",["Pop"]));
+
+	//T
+	songs.push(new Song("Tajdar E Haram",["Atif Aslam"],"Hindi",["Sufi"]));
+	songs.push(new Song("Take Me Home, Country Roads",["John Denver"],"English",["Country"]));
+	songs.push(new Song("Tera Chehra",["Adnan Sami"],"Hindi",["Bollywood"]));
+	songs.push(new Song("Teri Khair Mangdi",["Bilal Saeed"],"Punjabi",["Pop","Bollywood"]));
+		
+	//U
+	songs.push(new Song("Unforgettable",["French Montana", "Swae Lee"],"English",["Hip Hop"]));
+
+	//V
+
+
+	//W
+	songs.push(new Song("Whistle",["Flo Rida"],"English",["Hip Hop"]));
+
+	//X
+
+	//Y
+	songs.push(new Song("Ya Lili",["Balti","Hamouda"],"Arabic",["Hip Hop"]));
+	songs.push(new Song("Ye Kaali Kaali Aankhen",["Kumar Sanu","Anu Malik"],"Hindi",["Bollywood"]));
+
+	//Z
+	
+}
+
+function songclick(i){
+		var x= document.getElementById("audio-4");
+		x.src = "" +songs[i].srce;
+		x.onload = x.play();
+		getRandomBackground(1,10);
+		getlyrics(i);
+		loopplaylist();
+}
+
+function update_cs(ccpi){
+	cs = ccpi;
+}
+
+var pr=-1;
+function getlyrics(i){
+    if (pr!=-1){
+	 document.getElementById(songs[pr].name).style.display = "none";
+	} else {
+		document.getElementById("init").style.display = "none";
+	}
+	pr=i;
+	if (document.getElementById(songs[i].name)!=null){
+	document.getElementById(songs[i].name).style.display = "block";
+	} else {
+		pr=-1;
+		document.getElementById("init").style.display = "block";
+		
+	}
+}
+
+function sdesc(i){
+	text= "" + songs[i].name ;
+	return text;
+}
+
+
+function getRandomBackground(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    var x= Math.floor(Math.random() * (max - min + 1)) + min;
+	document.body.style.background = "url('bg/" + x + ".jpg') center top fixed no-repeat";
+}
+
+
+
+
+var cs; //current song playing curplaylist-index
+var curplaylist;
+var ccpi; // to update song index in curplaylist 
+//function for loading tracks in playlist
+function loadtrack(){
+	if (arguments[0]==1){
+		curplaylist=[];
+		cs=-1;
+		ccpi=0;
+		//all songs table
+		var i;
+		var text="<div class=\"ad-hide-large\" style=\"margin-top:83px\"></div><table>";
+		for(i=0;i<songs.length;i++) {
+			curplaylist.push(i);
+			text+="<tr><td onclick=songclick(" + i + ");update_cs(" + ccpi + ");>" + songs[i].name + "<br><small>" 
+				+songs[i].artist + "</small></td></tr>";
+			ccpi+=1;
+		}
+		text+="</table>";
+		document.getElementById("playlist").innerHTML = text;
+	}
+	else if (arguments[0]==2){
+		curplaylist=[];
+		cs=-1;
+		ccpi=0;
+		//songs sorted by language
+		var i;
+		var text="<div class=\"ad-hide-large\" style=\"margin-top:83px\"></div><table>";
+		for(i=0;i<songs.length;i++) {
+			if (songs[i].language==arguments[1]){
+				curplaylist.push(i);
+				text+="<tr><td onclick=songclick(" + i + ");update_cs(" + ccpi + ");>" + songs[i].name + "<br><small>" 
+					+songs[i].artist + "</small></td></tr>";
+				ccpi+=1;
+			}
+		}
+		text+="</table>";
+		document.getElementById("playlist").innerHTML = text;
+		
+	}
+	else if (arguments[0]==3){
+		curplaylist=[];
+		cs=-1;
+		ccpi=0;
+		//songs sorted by language
+		var i;
+		var text="<div class=\"ad-hide-large\" style=\"margin-top:83px\"></div><table>";
+		for(i=0;i<songs.length;i++) {
+			var j;
+			for(j=0;j<songs[i].genre.length;j++) {
+			if (songs[i].genre[j]==arguments[1]){
+				curplaylist.push(i);
+				text+="<tr><td onclick=songclick(" + i + ");update_cs(" + ccpi + ");>" + songs[i].name + "<br><small>" 
+					+songs[i].artist + "</small></td></tr>";
+				ccpi+=1;
+			}
+			}
+		}
+		text+="</table>";
+		document.getElementById("playlist").innerHTML = text;
+		
+	}
+	ad_close();
+}
+function loopplaylist(){
+var aud = document.getElementById("audio-4");
+aud.onended = function() {
+    cs=(cs+1)%curplaylist.length;
+    songclick(curplaylist[cs]);
+};
+}
