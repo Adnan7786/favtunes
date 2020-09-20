f098ec1d39a9a5ae2167e95dcd42d38f25f828c5
Md Adnan Akhtar <64841826+Adnan7786@users.noreply.github.com>
2020-09-20 13:28:40 +0530

    Update adscript.js

--- adscript.js
+++ adscript.js
@@ -172,6 +172,7 @@ function songclick(i){
 		var x= document.getElementById("audio-4");
 		x.src = "" +songs[i].srce;
 		x.onload = x.play();
+	        random_bg_color();
 		getRandomBackground(1,10);
 		getlyrics(i);
 		loopplaylist();
@@ -203,6 +204,20 @@ function sdesc(i){
 	return text;
 }
 
+function random_bg_color() {
+
+  // Get a number between 64 to 256 (for getting lighter colors)
+  let red = Math.floor(Math.random() * 256) + 64;
+  let green = Math.floor(Math.random() * 256) + 64;
+  let blue = Math.floor(Math.random() * 256) + 64;
+
+  // Construct a color withe the given values
+  let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
+
+  // Set the background to that color
+  document.body.style.background = bgColor;
+  //document.getElementById("dJ").style.background = bgColor;
+}
 
 function getRandomBackground(min, max) {
     min = Math.ceil(min);
